3adb4c7504366b946f788de2f065a105f91c0d93
lillie <lillie@Lillies-MacBook-Pro.local>
2021-06-11 00:21:20 -0700

    added array of questions

--- index.js
+++ index.js
@@ -1,13 +1,103 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+  .prompt([
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Please provide a description of your application/project.',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'If applicable, please provide the steps required to install your application.',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Please provide the contribution guidelines for the future contribuitor?',
+        name: 'contributor',
+    },
+    {
+        type: 'input',
+        message: 'Provide instructions and examples for use.(if you have photo/video, you can provide a link in next question.)',
+        name: 'usage',
+    },
+    {
+        type: "input",
+        message: "Please provide a link of a video or photo to demonstrate the application's use.",
+        name: "media",
+    },
+    {
+        type: "input",
+        message: "How did you test this application?",
+        name: "test",
+    },
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "github",
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email",
+    },
+    {
+        type: 'list',
+        message: 'Choose a license for your application/project.',
+        choice: [
+            'Academic Free License v3.0',
+            'Apache license 2.0',
+            'Artistic license 2.0',
+            'Boost Software License 1.0',
+            'BSD 2-clause "Simplified" license',
+            'BSD 3-clause "New" or "Revised" license',
+            'BSD 3-clause Clear license',
+            'Creative Commons license family',
+            'Creative Commons Zero v1.0 Universal',
+            'Creative Commons Attribution 4.0',
+            'Creative Commons Attribution Share Alike 4.0',
+            'Do What The F*ck You Want To Public License',
+            'Educational Community License v2.0',
+            'Eclipse Public License 1.0',
+            'Eclipse Public License 2.0',
+            'European Union Public License 1.1',
+            'GNU Affero General Public License v3.0',
+            'GNU General Public License family',
+            'GNU General Public License v2.0',
+            'GNU General Public License v3.0',
+            'GNU Lesser General Public License family',
+            'GNU Lesser General Public License v2.1',
+            'GNU Lesser General Public License v3.0',
+            'ISC', 'LaTeX Project Public License v1.3c',
+            'Microsoft Public License', 'MIT',
+            'Mozilla Public License 2.0',
+            'Open Software License 3.0',
+            'PostgreSQL License',
+            'SIL Open Font License 1.1',
+            'University of Illinois/NCSA Open Source License',
+            'The Unlicense',
+            'zLib License'
+        ],
+        name: 'license'
+    }
+
+
+]);
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
