11837696f910da912a3f1c1373070548e0ad74c2
lillie <lillie@Lillies-MacBook-Pro.local>
2021-06-11 01:24:28 -0700

    added README contect

--- index.js
+++ index.js
@@ -2,7 +2,48 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// TODO: Create an array of questions for user input
+// Readme contect that will display on README.md 
+const generateReadme = (answers) =>
+
+`# ${answers.title}
+![GitHub license](https://img.shields.io/badge/license-${answers.license}-yellow.svg) 
+
+## Description${answers.description}
+
+## Table of Contents 
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+*Steps required to install project and get the development environment established:*
+${answers.installation}
+
+## Usage
+*Instructions and examples for use:* 
+[Click Here](${answers.usage}), for the instruction's video on how to use this application.
+
+
+## License
+Built under the ${answers.license} licence. 
+
+## Contributing
+${answers.contributing}
+
+## Tests
+${answers.test}
+
+## Questions
+If you have any questions or want to make a contribution please reach out to me here: 
+
+* [GitHub:](https://github.com/${answers.github}/).  
+* Email: ${answers.email}.`
+;
+
+// Create an array of questions for user input
 inquirer
   .prompt([
     {
@@ -23,17 +64,12 @@ inquirer
     {
         type: 'input',
         message: 'Please provide the contribution guidelines for the future contribuitor?',
-        name: 'contributor',
+        name: 'contributing',
     },
     {
         type: 'input',
-        message: 'Provide instructions and examples for use.(if you have photo/video, you can provide a link in next question.)',
+        message: 'Provide instructions and examples for use. Include screenshots or video as needed',
         name: 'usage',
-    },
-    {
-        type: "input",
-        message: "Please provide a link of a video or photo to demonstrate the application's use.",
-        name: "media",
     },
     {
         type: "input",
@@ -53,51 +89,27 @@ inquirer
     {
         type: 'list',
         message: 'Choose a license for your application/project.',
-        choice: [
-            'Academic Free License v3.0',
-            'Apache license 2.0',
-            'Artistic license 2.0',
-            'Boost Software License 1.0',
-            'BSD 2-clause "Simplified" license',
-            'BSD 3-clause "New" or "Revised" license',
-            'BSD 3-clause Clear license',
-            'Creative Commons license family',
-            'Creative Commons Zero v1.0 Universal',
-            'Creative Commons Attribution 4.0',
-            'Creative Commons Attribution Share Alike 4.0',
-            'Do What The F*ck You Want To Public License',
-            'Educational Community License v2.0',
-            'Eclipse Public License 1.0',
-            'Eclipse Public License 2.0',
-            'European Union Public License 1.1',
-            'GNU Affero General Public License v3.0',
-            'GNU General Public License family',
-            'GNU General Public License v2.0',
-            'GNU General Public License v3.0',
-            'GNU Lesser General Public License family',
-            'GNU Lesser General Public License v2.1',
-            'GNU Lesser General Public License v3.0',
-            'ISC', 'LaTeX Project Public License v1.3c',
-            'Microsoft Public License', 'MIT',
-            'Mozilla Public License 2.0',
-            'Open Software License 3.0',
-            'PostgreSQL License',
-            'SIL Open Font License 1.1',
-            'University of Illinois/NCSA Open Source License',
-            'The Unlicense',
-            'zLib License'
-        ],
+        choice: ['GPL','LGPL','ISC','MIT','POSTGRESQL','NCSA','UNLICENSE','ZLIB'],
         name: 'license'
     }
+])
 
 
-]);
+.then((answer) => {
+    const readmeContect = generateReadme(answer);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+    fs.writeFile('README.md', readmeContect, (err) =>
+    err ? console.log(err) : console.log('You successfully created README.md!')
+    );
+  });
 
-// TODO: Create a function to initialize app
-function init() { }
 
-// Function call to initialize app
-init();
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) { }
+
+// // TODO: Create a function to initialize app
+// function init() { }
+
+// // Function call to initialize app
+// init();
